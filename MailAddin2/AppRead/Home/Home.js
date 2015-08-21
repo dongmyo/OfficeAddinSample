1617c5702e47de39f264bf3232ffff87f3897402
신동민/NE <dongmyo@nhnent.com>
2015-08-21 13:46:46 +0900

    Offie Add-ins Sample: a Mail Add-in

---
+++ MailAddin2/AppRead/Home/Home.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../App.js" />
+/*global app*/
+(function() {
+    'use strict';
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function(reason) {
+        $(document).ready(function() {
+            app.initialize();
+            getDoorayTaskRef();
+
+        });
+    };
+})();
+
+function getDoorayTaskRef() {
+	var regEx = /#([\w\-]+)\/([\d]+)/g;
+	
+	var content = "";
+	
+	var matches = Office.context.mailbox.item.getRegExMatchesByName("BodyFilter");
+	for(var i = 0; i < matches.length; i++) {
+		var arrResult = regEx.exec(matches[i]);
+		var url = "http://nhnent.dooray.com/popup/task/projects/" + arrResult[1] + "/" + arrResult[2];
+		
+		content += "<a href='" + url + "' target='_blank'>Open Dooray Task: " + arrResult[0] + "</a><br />";
+	}
+	
+	$('#doorayArea').empty();
+	$('#doorayArea').append($(content));
+}
